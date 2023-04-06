383110fec964233804c4e01b339b89ff60b54110
izada <51282126+izada@users.noreply.github.com>
2023-04-06 22:24:56 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 //  slider js
 $(document).ready(function(){
     $('.topclient').slick({
-        slidesToShow: 6,
+        slidesToShow: 5,
         slidesToScroll: 1,
         dots: false,
         arrows: false,
@@ -43,6 +43,40 @@ $(document).ready(function(){
   });
 
 
+$(document).ready(function(){
+    $('.videoplayer').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        prevArrow:'<div class="left-slide arrow"><i class="fa-solid fa-angle-left"></i></div>',
+        nextArrow:'<div class="right-slide arrow"><i class="fa-solid fa-angle-right"></i><div>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                autoplay: true,
+                arrows: false,
+                autoplaySpeed: 2000,
+                infinite: true,
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                slidesToShow: 1
+              }
+            }
+          ]
+    });
+  });
+
+
   // counter js
   
  $(document).ready(function($) {
@@ -50,4 +84,21 @@ $(document).ready(function(){
         delay: 10,
         time: 1000
     });
+});
+
+// popup slider
+$(document).ready(function() {
+  $('.magniView').magnificPopup({
+		type: 'image',
+		closeOnContentClick: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true
+		},
+    gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+  });
 });
